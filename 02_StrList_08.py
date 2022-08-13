042ae0893c5cdcc79bfc18964da90d9fc45f6b36
Phatorn Pitiphat <ph.pitiphat@gmail.com>
2022-08-14 02:01:06 +0700

    update solutions

---
+++ 02_StrList_08.py
@@ -0,0 +1,19 @@
+import math
+
+
+a, b, c = input().strip().split(",")
+if b == "":
+    dividend = int(c)
+else:
+    dividend = int(b + c) - int(b)
+nine = zero = ""
+for i in range(len(c)):
+    nine = nine + "9"
+for i in range(len(b)):
+    zero = zero + "0"
+divisor = int(nine + zero)
+dividend = divisor * int(a) + dividend
+gcd = int(math.gcd(dividend, divisor))
+dividend = int(dividend / gcd)
+divisor = int(divisor / gcd)
+print(dividend, "/", divisor)
---
+++ 02_StrList_F06.py
@@ -0,0 +1,7 @@
+def add_vector(u, v):
+    u = list(u)
+    v = list(v)
+    return [u[0] + v[0], u[1] + v[1], u[2] + v[2]]
+
+
+exec(input())  # DON'T remove this line

facc0ed532db27adc6e0f8937e06004da31b37c0
Phatorn Pitiphat <ph.pitiphat@gmail.com>
2022-08-24 20:47:44 +0700

    add solutions

---
+++ 02_StrList_F02.py
@@ -0,0 +1,26 @@
+def number_name(n):
+    n = int(n)
+    if n == 0:
+        return "zero"
+    elif n == 1:
+        return "one"
+    elif n == 2:
+        return "two"
+    elif n == 3:
+        return "three"
+    elif n == 4:
+        return "four"
+    elif n == 5:
+        return "five"
+    elif n == 6:
+        return "six"
+    elif n == 7:
+        return "seven"
+    elif n == 8:
+        return "eight"
+    else:
+        n == 9
+        return "nine"
+
+
+exec(input())  # DON'T remove this line

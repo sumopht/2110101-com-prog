04bc0fe8d385e87053fc917f49b438317a1019e9
Phatorn Pitiphat <ph.pitiphat@gmail.com>
2022-08-30 22:33:23 +0700

    restructure repo

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-n = "12345"
-n = [*n]
-print(n)
+counts = dict()
+chars = [*input()]
+for char in chars:
+    if char >= 65 and char <= 90:
+        char += 32
+    if char not in counts:
+        counts[char] = 1
+    else:
+        counts[char] += 1
+print(counts)

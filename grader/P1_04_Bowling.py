35b628ae65a7dccc587d1f3c197f655604461203
Phatorn Pitiphat <ph.pitiphat@gmail.com>
2022-09-17 00:29:04 +0700

    add solutions

--- grader/P1_04_Bowling.py
+++ grader/P1_04_Bowling.py
@@ -1,34 +1,29 @@
-def convert(c):
-    return 10 if not ("0" <= c <= "9") else int(c)
+result = input().strip()
+target = int(input())
+i = 0 # เตรียมตัวแปรที่คอยเก็บ index เริ่มต้นของเฟรม
+total_score = 0 # เตรียมตัวแปรเก็บคะแนนรวม
+for f in range(1,11) : # บังคับให้ท ำตั้งแต่เฟรม 1,2,...,10
+ # ต้องให้ i เกบ็ ตำ แหน่งเริ่มต้นของเฟรม f ใน result
+ เขียนแบบตรงไปตรงมำ
+ ก็แยกเป็ น 7 กรณี XXX, XX?, X?/, X??, ?/X, ?/?, ??
+ เพื่อค ำนวณคะแนนของเฟรม f เก็บใส่ตัวแปร score_in_frame_f
+ แต่ละกรณีอำจมีรูปแบบกำรค ำนวณคะแนน และปรับค่ำของ i ต่ำงกัน
+ if ..... :
+ ...
+ elif ... :
+ ...
+ elif ... :
+ ...
+ elif ... :
+ ...
+ elif ... :
+ ...
+ elif ... :
+ ...
+ else :
+ ...
 
-
-s = input()
-n = int(input())
-score = [0 for e in range(10)]
-inx = 0
-i = 0
-total = 0
-while (i < len(s)):
-    if inx == 9:
-        for j in range(i, len(s)):
-            score[inx] += convert(s[j])
-            if (s[j] == '/'):
-                score[inx] -= convert(s[j-1])
-        for j in score:
-            total += j
-        break
-    else:
-        if s[i] == 'X':
-            score[inx] = 10 + convert(s[i+1]) + convert(s[i+2])
-            if s[i+2] == '/':
-                score[inx] -= convert(s[i+1])
-            inx += 1
-            i += 1
-        else:
-            score[inx] += convert(s[i]) + convert(s[i+1]) + (convert(s[i+2]) if s[i+1] == '/' else 0)
-            if s[i+1] == '/':
-                score[inx] -= convert(s[i])
-            inx += 1
-            i += 2
-
-print("{}".format(score[n-1] if 1 <= n <= 10 else total))
+ total_score += score_in_frame_f
+ if f == target :
+ ???
+???
